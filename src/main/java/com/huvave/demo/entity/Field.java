a8b9a847d29d80a22ab59cb408e0218ff60e90c1
grenudi <idunerg@gmail.com>
2019-01-25 18:58:27 +0300

    init

---
+++ src/main/java/com/huvave/demo/entity/Field.java
@@ -0,0 +1,21 @@
+package com.huvave.demo.entity;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "fields")
+public class Field extends BaseEntity{
+
+    @OneToMany(mappedBy = "field")
+    private List<Main> mains;
+
+    public List<Main> getMains() {
+        return mains;
+    }
+
+    public void setMains(List<Main> mains) {
+        this.mains = mains;
+    }
+}
---
+++ src/main/java/com/huvave/demo/entity/Main.java
@@ -0,0 +1,32 @@
+package com.huvave.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "main12")
+public class Main extends BaseEntity {
+
+    @Column(columnDefinition = "VARCHAR(280)")
+    private String label;
+
+    @ManyToOne
+    @JoinColumn(name = "field_id", nullable = false)
+    private Field field;
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
+
